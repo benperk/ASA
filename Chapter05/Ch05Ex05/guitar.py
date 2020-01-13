65ab0f7058e7a9b8706f693834d572691ce902e6
Benjamin Perkins <benperk@microsoft.com>
2020-01-13 18:11:35 +0100

    Update guitar.py

--- Chapter05/Ch05Ex05/guitar.py
+++ Chapter05/Ch05Ex05/guitar.py
@@ -14,3 +14,20 @@ def get_fender_item():
         'OriginalOwner': 'csharpguitar'
     }
     return fender_item
+
+# ---- Add this JSON document manually in the portal, remove the pound/sharp sign ----
+# --------------- See Figure 5-35 as an example of how it should look ----------------
+#{
+#    "id": "1",
+#    "Description": [
+#    {
+#        "Brand": "Fender",
+#        "Model": "Stratocaster",
+#        "Year": 1956,
+#        "Color": "Sunburst",
+#        "Condition": "Mint",
+#        "Accessories": "Case,Strings,Picks"
+#    }            
+#    ],
+#    "OriginalOwner": "csharpguitar"
+#}

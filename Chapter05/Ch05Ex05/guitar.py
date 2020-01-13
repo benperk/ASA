46b7b846b28821a979398115df2b72550402afa8
Benjamin Perkins <benperk@microsoft.com>
2020-01-13 15:49:10 +0100

    Create guitar.py

---
+++ Chapter05/Ch05Ex05/guitar.py
@@ -0,0 +1,16 @@
+def get_fender_item():
+    fender_item = {
+        'id': '1',
+        'Description': [
+            {
+                'Brand': 'Fender',
+                'Model': 'Stratocaster',
+                'Year': 1956,
+                'Color': 'Sunburst',
+                'Condition': 'Mint',
+                'Accessories': 'Case,Strings,Picks'
+            }            
+        ],
+        'OriginalOwner': 'csharpguitar'
+    }
+    return fender_item

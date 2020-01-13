65bfe79e819c4a8b9689f8cfec3568ce2226a129
Benjamin Perkins <benperk@microsoft.com>
2020-01-13 18:11:05 +0100

    Update csharp.py

--- Chapter05/Ch05Ex05/csharp.py
+++ Chapter05/Ch05Ex05/csharp.py
@@ -14,3 +14,21 @@ def get_csharp_item():
         'DevelopedBy': 'Microsoft'
     }
     return csharp_item
+
+
+# ---- Add this JSON document manually in the portal, remove the pound/sharp sign ----
+# --------------- See Figure 5-35 as an example of how it should look ----------------
+#{
+#    "id": "2",
+#    "Description": [
+#        {
+#            "Name": "C#",
+#            "Pronunciation": "see sharp",
+#            "Year": 2000,
+#            "TargetOS": "Windows",
+#            "Crossplatform": true,
+#            "CurrentVersion": 8.0
+#        }            
+#    ],
+#    "DevelopedBy": "Microsoft"
+#}

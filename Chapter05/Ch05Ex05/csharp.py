4bc561dac7c8c5614645875a08f11e7a37cd6dd4
Benjamin Perkins <benperk@microsoft.com>
2020-01-13 15:49:28 +0100

    Create csharp.py

---
+++ Chapter05/Ch05Ex05/csharp.py
@@ -0,0 +1,16 @@
+def get_csharp_item():
+    csharp_item = {
+        'id': '2',
+        'Description': [
+            {
+                'Name': 'C#',
+                'Pronunciation': 'see sharp',
+                'Year': 2000,
+                'TargetOS': 'Windows',
+                'Crossplatform': True,
+                'CurrentVersion': 8.0
+            }            
+        ],
+        'DevelopedBy': 'Microsoft'
+    }
+    return csharp_item
